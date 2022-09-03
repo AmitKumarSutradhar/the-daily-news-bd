a99eadd58743b10ccd7504724e0a0f0f64f46113
Amit Kumar <amit596450@gmail.com>
2022-09-03 13:25:08 +0600

    Main Section Added and Some Issues fixing

--- js/app.js
+++ js/app.js
@@ -1,35 +1,59 @@
-const loadAllProducts = async () => {
-    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
-    const data = await response.json();
-    // console.log(data);
-    return data;
+// Display Catagory News Details 
+const loadAllNews = (newsId) =>{
+    // fetch('https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a')
+    fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
+    .then(res => res.json())
+    .then(data => displayAllNews(data.data[0]));
 }
 
+const displayAllNews = allNews => {
+    const newsDetails = document.getElementById("newsDetails");
+    const newsItem = document.createElement("div");
+    // newsItem.innerHTML = `
+    //     <div class="col-lg-12 col-md-12 col-12">
+    //                     <div class="card mb-3">
+    //                         <div class="row g-0">
+    //                             <div class="col-md-4 col-sm-12">
+    //                                 <img src="${allNews.image_url}" class="img-fluid w-100 rounded-start" alt="...">
+    //                             </div>
+    //                             <div class="col-md-8">
+    //                                 <div class="card-body">
+    //                                     <h5 class="card-title">${allNews.title}</h5>
+    //                                     <p class="card-text">${allNews.details}</p>
+    //                                     <div class="card-meta d-flex">
+    //                                         <div class="author-info">
+    //                                             <div><img src="${allNews.author.img} class="img-fluid w-100 rounded-start" width ="50px" alt=""></div>
+                                                
+    //                                             <div class="author-details">
+    //                                                 <h3 class="author-name">${allNews.author.name}</h3>
+    //                                                 <p class="author-blog-date"></p>
+    //                                             </div>
+    //                                         </div>
+    //                                     </div>
+                                        
+    //                                 </div>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    // `;
+    newsDetails.appendChild(newsItem);
 
-const setAllMenu = async () => {
-
-    const data = await loadAllProducts();
-    console.log(data);
-    // const menu = document.getElementById("all-menu");
-
-    // const uniqueArray = [];
-
-    for (const news in data) {
-        console.log(news.news_category.category_name)
-    //     // console.log(uniqueArray.indexOf(product.category))
-
-    //     if (uniqueArray.indexOf(product.category) === -1) {
-
-    //         uniqueArray.push(product.category);
-
-    //         const li = document.createElement("li");
-    //         li.innerHTML = `<a>${product.category}</a>`;
-    //         menu.appendChild(li);
-    //     }
-    }
-
-    // console.log(uniqueArray)
+    console.log(allNews);
 }
 
-setAllMenu();
-// loadAllProducts();
+// const loadAllNewsDetails = (newsId) => {
+//     fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
+//     .then(res => res.json())
+//         .then(data => console.log(data));
+//     // console.log(allNews);
+// };
+// loadAllNewsDetails();
+loadAllNews('${data.data[0])}');
+
+
+
+// Display Catagories Name Item 
+
+
+
