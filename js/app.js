bdc27d9fbd05af42a5623db8952dd904ca6e6368
Amit Kumar <amit596450@gmail.com>
2022-09-04 00:26:14 +0600

    Main Section Daynamic Data Fetch

--- js/app.js
+++ js/app.js
@@ -3,55 +3,53 @@ const loadAllNews = (newsId) =>{
     fetch('https://openapi.programming-hero.com/api/news/category/01')
     .then(res => res.json())
     .then(data => displayAllNews(data.data));
+
 }
 
 const displayAllNews = allNews => {
-    // console.log(allNews);
-    const newsItem = document.createElement("div");
+    console.log(allNews.length);
+
+    const totalItemContainer = document.getElementById('total-item');
+    totalItemContainer.innerText  = allNews.length;
+
+    const newsContainer = document.getElementById('newsDetails');
     allNews.forEach(newsDetails => {
-        console.log(newsDetails);
+        // console.log(newsDetails.author.name);
+        const newsItem = document.createElement("div");
+
+        newsItem.innerHTML = `
+        <div class="col-lg-12 col-md-12 col-12">
+                        <div class="card mb-3">
+                            <div class="row g-0">
+                                <div class="col-md-4 col-sm-12">
+                                    <img src="${newsDetails.image_url}" class="img-fluid w-100 rounded-start" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${newsDetails.title}</h5>
+                                        <p class="card-text">${newsDetails.details}</p>
+                                        <div class="card-meta d-flex">
+                                            <div class="author-info">
+                                                <div><img src="${newsDetails.author.img} class="img-fluid w-100 rounded-start" width ="50px" alt=""></div>
+                                                
+                                                <div class="author-details">
+                                                    <h3 class="author-name">${newsDetails.author.name}</h3>
+                                                    <p class="author-blog-date"></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    `;
+        newsContainer.appendChild(newsItem);
         
     });
-    // newsItem.innerHTML = `
-    //     <div class="col-lg-12 col-md-12 col-12">
-    //                     <div class="card mb-3">
-    //                         <div class="row g-0">
-    //                             <div class="col-md-4 col-sm-12">
-    //                                 <img src="${allNews.image_url}" class="img-fluid w-100 rounded-start" alt="...">
-    //                             </div>
-    //                             <div class="col-md-8">
-    //                                 <div class="card-body">
-    //                                     <h5 class="card-title">${allNews.title}</h5>
-    //                                     <p class="card-text">${allNews.details}</p>
-    //                                     <div class="card-meta d-flex">
-    //                                         <div class="author-info">
-    //                                             <div><img src="${allNews.author.img} class="img-fluid w-100 rounded-start" width ="50px" alt=""></div>
-                                                
-    //                                             <div class="author-details">
-    //                                                 <h3 class="author-name">${allNews.author.name}</h3>
-    //                                                 <p class="author-blog-date"></p>
-    //                                             </div>
-    //                                         </div>
-    //                                     </div>
-                                        
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    // `;
-    // newsDetails.appendChild(newsItem);
-
-    // console.log(allNews);
 }
 
-// const loadAllNewsDetails = (newsId) => {
-//     fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
-//     .then(res => res.json())
-//         .then(data => console.log(data));
-//     // console.log(allNews);
-// };
-// loadAllNewsDetails();
 loadAllNews();
 
 
--- js/catlist.js
+++ js/catlist.js
@@ -6,26 +6,10 @@ const loadCatList = (id) => {
 
 
 const loadCatListItem = catlist => {
-    // console.log(catlist);
-    // const modalTitle = document.getElementById('phoneDetailModalLabel');
-    // modalTitle.innerText = phone.name;
-    // const phoneDetails = document.getElementById('phone-details');
-    // console.log(phone.mainFeatures.sensors[0]);
-    // phoneDetails.innerHTML = `
-    //     <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
-    //     <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Information '}</p>
-    //     <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
-    //     <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
-    // `
-
-
-
     const newsContainer = document.getElementById('all-menu-item');
     catlist.forEach(news => {
         // console.log(news);
         const li = document.createElement('li');
-        // li.classList.add('d-flex');
-        // li.classList.add('flex-column');
         li.innerHTML = `
             <a class="text-decoration-none text-secondary py-5" href="" onclick="loadAllNews();">${news.category_name}</a>
         `;
@@ -33,5 +17,4 @@ const loadCatListItem = catlist => {
     });
 }
 
-// console.log("Hello from Catlist");
 loadCatList();
