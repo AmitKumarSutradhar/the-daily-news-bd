a73ea8e909a8197d8deccf40985b7b1ea1e06050
Amit Kumar <amit596450@gmail.com>
2022-09-04 09:27:44 +0600

    Main Section Massive Issue Fix

--- js/catlist.js
+++ js/catlist.js
@@ -42,23 +42,32 @@ const displayAllNews = allNews => {
         newsItem.innerHTML = `
         <div class="col-lg-12 col-md-12 col-12">
                         <div class="card mb-3">
-                            <div class="row g-0">
-                                <div class="col-md-4 col-sm-12">
+                            <div class="row g-0 align-items-center">
+                                <div class="col-md-4 col-sm-12 p-3">
                                     <img src="${newsDetails.image_url}" class="img-fluid w-100 rounded-start" alt="">
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body">
                                         <h5 class="card-title">${newsDetails.title}</h5>
-                                        <p class="card-text">${newsDetails.details}</p>
-                                        <div class="card-meta d-flex">
-                                            <div class="author-info">
-                                                <div><img src="${newsDetails.author.img} class="img-fluid w-100 rounded-start" width ="50px" alt=""></div>
+                                        <p class="card-text">${(newsDetails.details).slice(0, 600) + "..."}</p>
+                                        <div class="card-meta d-flex justify-content-between">
+                                            <div class="author-info d-flex">
+                                                <div class="me-3"><img src="${newsDetails.author.img} class="rounded-circle img-fluid w-100" width ="50px" alt=""></div>
                                                 
+
                                                 <div class="author-details">
-                                                    <h3 class="author-name">${newsDetails.author.name}</h3>
-                                                    <p class="author-blog-date"></p>
+                                                    <h3 class="author-name">${newsDetails.author.name ? newsDetails.author.name : 'No data Available'}</h3>
+                                                    <p class="author-blog-date">${newsDetails.author.published_date}</p>
                                                 </div>
                                             </div>
+                                            <div class="news-total-view">
+                                                    <h3 class="author-name fw-bold"><i class="fa-regular fa-eye"></i> ${newsDetails.total_view ? newsDetails.total_view : 'No data Available'}</h3>
+                                                    <p class="author-blog-date"></p>
+                                            </div>
+                                            <div class="">
+                                                    <i class="fa-solid fa-arrow-right"></i>
+                                            </div>
+
                                         </div>
                                         
                                     </div>
